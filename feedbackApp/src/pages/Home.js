264c3b943c294db58ebc2f9795b496badd394c1d
Abby <biabbye@gmail.com>
2021-12-08 00:45:50 +0200

    Landing page design first commit

--- feedbackApp/src/pages/Home.js
+++ feedbackApp/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout } from "../layout";
+import "./Home.css";
 
 const Home = () => {
   console.log("11111");
   return (
     <Layout>
-      <h1 className="text-2xl">Home</h1>
-      <p>uuu</p>
+      <h1 className="text-5xl homeTitle">Rate your course!</h1>
+      <p></p>
     </Layout>
   );
 };
