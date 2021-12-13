f409d902b468a36a8af3afcee214217b61972be9
Stela Florea <stela@Stelas-MacBook-Pro.local>
2021-12-14 00:25:55 +0200

    removed service worker for now

--- feedbackApp/src/index.js
+++ feedbackApp/src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import registerServiceWorker from "./registerServiceWorker";
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
@@ -17,5 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-registerServiceWorker.unregister();
--- feedbackApp/src/layout/SignUp.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const SignUp = () => (
-  <div>
-    <h3>React SignUp Component</h3>
-  </div>
-);
-export default SignUp;
