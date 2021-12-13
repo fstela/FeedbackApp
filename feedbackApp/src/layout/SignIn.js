44db2499ab5e2494355467bbe6ce53c4aee28eab
DavidGafitaGit <gafitadavid19@stud.ase.ro>
2021-12-13 23:50:23 +0200

    SignUp + SignIn

---
+++ feedbackApp/src/layout/SignIn.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SignIn = () => (
+  <div>
+    <h3>React SignIn Component</h3>
+  </div>
+);
+export default SignIn;
---
+++ feedbackApp/src/layout/SignUp.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SignUp = () => (
+  <div>
+    <h3>React SignUp Component</h3>
+  </div>
+);
+export default SignUp;
